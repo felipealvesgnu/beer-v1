6ea03b9ff438d1603430b16fbbf304305bebd588
Felipe Alves Lourenço Pereira <felipealves.gnu@gmail.com>
2012-07-03 14:50:24 -0300

    Classes recompiladas.XX

--- src/com/example/web/BeerSelect.java
+++ src/com/example/web/BeerSelect.java
@@ -1,8 +1,11 @@
 package com.example.web;
 
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+
+import com.example.model.BeerExpert;
 
 
 public class BeerSelect extends HttpServlet{
@@ -12,7 +15,16 @@ public class BeerSelect extends HttpServlet{
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("Beer Selection Advice<br>");
+	
 		String c = request.getParameter("color");
-		out.println("<br>Got beer color " + c);
+		BeerExpert be = new BeerExpert();
+		List result = be.getBrands(c);
+		//out.println("<br>Got beer color " + c);
+		out.println("<br>Beer Selection advice " + c);
+
+		Iterator it = result.iterator();
+		while(it.hasNext()){
+			out.print("<br/>try: " + it.next());
+		}
 	}
 }
